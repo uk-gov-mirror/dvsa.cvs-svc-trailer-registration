f9a6dea6aaa6fa0367309d425aeab54c5937676f
m-ujaffer <54844952+m-ujaffer@users.noreply.github.com>
2021-03-30 17:38:00 +0100

    Initial commit

---
+++ scripts/webpack.config.build.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+// const nodeExternals = require('webpack-node-externals');
+
+const { NODE_ENV, SERVICE, AWS_PROVIDER_REGION, AWS_PROVIDER_STAGE } = process.env;
+
+if (!SERVICE)
+  throw new Error(
+    `"SERVICE" environmental variable not provided.\n
+    Please provide it so your application can stop proxying requests and use a base path.`,
+  );
+
+console.log(
+  `\nBuilding Service:\n '${SERVICE}'\n mode: ${NODE_ENV}\n stage: '${AWS_PROVIDER_STAGE}'\n region: '${AWS_PROVIDER_REGION}'\n\n`,
+);
+
+module.exports = {
+  context: path.resolve(__dirname, '..'),
+  //TODO Reduce if multiple functions or multi bundle, outputs are required
+  entry: path.resolve(__dirname, '..', 'src/index.ts'),
+  target: 'node',
+  devtool: 'source-map',
+  stats: {
+    colors: true,
+    reasons: true,
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    libraryTarget: 'commonjs',
+    filename: 'handler.js',
+    path: path.resolve(__dirname, '..', '.build'),
+  },
+  externals: [],
+  plugins: [new CleanWebpackPlugin()],
+};
