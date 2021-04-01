386a7cd1ef068a310ea1b217492b7ed84eccbb87
jonlazarini <jlazarini@deloitte.co.uk>
2021-04-01 12:16:00 +0100

    feat(cvsb-18919): change cfg, add more instructions after successfully tested deployment

--- src/infrastructure/api/index.ts
+++ src/infrastructure/api/index.ts
@@ -4,10 +4,8 @@ const app = express();
 
 const router = express.Router();
 
-const { API_VERSION, SERVICE } = process.env;
+const { API_VERSION } = process.env;
 
-console.log({ SERVICE });
-console.log({ API_VERSION });
 /**
  * Define routing and route level middleware if necessary from ./routes
  * (POST) http://localhost:3009/<stage>/                                  (configured on AWS)
